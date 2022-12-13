92d0189492062219d0a4c32535c09b1a4c1b5af1
Fabian Herrera Martinez <contactofabianmartinez@gmail.com>
2022-12-13 00:42:10 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Principal from './components/Principal'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Principal />
+    </div>
+  )
+}
+
+export default App
