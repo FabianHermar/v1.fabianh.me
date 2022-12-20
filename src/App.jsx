037c885e3cf3f07f5e09bd2ddae51a3f4242031e
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2022-12-19 23:47:17 -0600

    all components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Header from './components/Header'
 import Principal from './components/Principal'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
       <Header />
       <Principal />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
