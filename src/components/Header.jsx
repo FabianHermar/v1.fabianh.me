631b2c0f2ea397e1b68433f45000a3444a1ed636
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2022-12-19 23:48:09 -0600

    all components added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
   const [menu, setMenu] = useState(false)
 
   const menuLinks = [
-    { name: 'Home', link: '#home' },
-    { name: 'About', link: '#about' },
-    { name: 'Skills', link: '#skills' },
-    { name: 'Projects', link: '#projects' },
-    { name: 'Contact', link: '#contact' }
+    { name: 'Inicio', link: '#home' },
+    { name: 'Sobre mi', link: '#about' },
+    { name: 'Habilidades', link: '#skills' },
+    { name: 'Proyectos', link: '#projects' },
+    { name: 'Contacto', link: '#contact' }
 
   ]
 
--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,4 +1,4 @@
-import FHLogo from '../../assets/FH.png'
+import FHLogo from '../../assets/principal.webp'
 
 function contact () {
   window.location.href = '#contact'
@@ -14,19 +14,19 @@ const Principal = () => {
   return (
     <section id='home' className='min-h-screen flex py-10 md:flex-row flex-col items-center'>
       <div className='flex-1 flex items-center justify-center h-full'>
-        <img src={FHLogo} className='md:w-11/12 h-full object-cover' alt='' />
+        <img src={FHLogo} className='md:w-11/12 h-full object-cover select-none' alt='' />
       </div>
       <div className='flex-1'>
         <div className='md:text-left text-center'>
           <h1 className='md:text-5xl text-2xl md:leading-normal leading-10'>
             <span className='font-[Tussila] text-gray-600 md:text-3xl text-2xl'>
-              Hello!, I'm
+              Hola 👋, Mi nombre es
               <br />
             </span>
             <span className='font-[Sequel] text-gray-800 md:text-6xl text-5xl'>Fabian Herrera</span>
           </h1>
-          <h3 className='font-[Tussila] text-gray-600 md:2xl text-xl md:leading-normal leading-5 mt-4'>I'm a <span>Frontend Developer</span></h3>
-          <button className='btn-contact mt-8' onClick={contact}>Contact Me</button>
+          <h3 className='font-[Tussila] text-gray-600 md:2xl text-xl md:leading-normal leading-5 mt-4'><span>Frontend Developer</span></h3>
+          <button className='btn-contact mt-8' onClick={contact}>Contacta Me</button>
           <div className='mt-8 text-3xl flex items-center md:justify-start justify-center gap-6'>
             {
               socialMedia?.map((social, icon) => (
