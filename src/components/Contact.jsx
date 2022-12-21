ebe72046c827aeac54a67166376654aa9e61e860
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2022-12-21 00:28:22 -0600

    component still unfinished

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,24 @@
-const Contact = () => {
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import toast, { Toaster } from 'react-hot-toast'
+
+export const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_suce7ty', 'template_pk4ch58', form.current, 'KXGnZ8ubQKs8gQ0C2')
+      .then((result) => {
+        console.log(result.text)
+      }, (error) => {
+        console.log(error.text)
+      })
+  }
+
   return (
-    <div>contact</div>
+    <section className='bg-dark_primary text-black' id='contact'>
+      contact
+    </section>
   )
 }
-
-export default Contact
