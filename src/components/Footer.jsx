8ab74cbe42480148964ba769aca4c6eae9d4ef1e
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2022-12-21 00:26:46 -0600

    Skills and Footer finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,32 @@
 const Footer = () => {
+  const socialMedia = [
+    { name: 'logo-linkedin', link: 'https://www.linkedin.com/in/fabianhmz/' },
+    { name: 'logo-github', link: 'https://github.com/FabianHMzz' },
+    { name: 'logo-twitter', link: 'https://www.twitter.com/fabianhmz_/' },
+    { name: 'mail', link: 'mailto:contactofabianmartinez@gmail.com' }
+  ]
+
   return (
-    <footer>
-      footer
+    <footer className='text-center bg-gray-800 text-white'>
+      <div className='container px-6 pt-4'>
+        <div className='flex justify-center mb-2'>
+          <div className='mt-4 text-3xl flex items-center md:justify-start justify-center gap-6'>
+            {
+              socialMedia?.map((social, icon) => (
+                <div key={icon} className='text-white hover:text-gray-400 ease-in duration-100 cursor-pointer'>
+                  <a href={social?.link} target='_blank' rel='noreferrer noopener'>
+                    <ion-icon name={social?.name}>...</ion-icon>
+                  </a>
+                </div>
+              ))
+             }
+          </div>
+        </div>
+      </div>
+
+      <div className='text-center p-4 font-[Tussila] text-sm'>
+        Desarrollado moviendo las manitas y con mucho ❤️ por <a className='font-extrabold underline decoration-sky-600 decoration-[4px] duration-200 hover:decoration-pink-600' href='https://github.com/FabianHMzz' target='_blank' rel='noreferrer noopener'>Fabian Herrera</a>
+      </div>
     </footer>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,64 @@
 const Skills = () => {
+  const skills = [
+    {
+      logo: '/assets/logos/HTML.webp',
+      title: 'HTML5',
+      level: 'Intermedio',
+      count: 65
+    },
+    {
+      logo: '/assets/logos/CSS.webp',
+      title: 'CSS3',
+      level: 'Intermedio',
+      count: 65
+    },
+    {
+      logo: '/assets/logos/JavaScript.webp',
+      title: 'JavaScript',
+      level: 'Principiante',
+      count: 32.5
+    },
+    {
+      logo: '/assets/logos/React.webp',
+      title: 'React',
+      level: 'Principiante',
+      count: 32.5
+    },
+    {
+      logo: '/assets/logos/TailwindCSS.webp',
+      title: 'TailwindCSS',
+      level: 'Principiante',
+      count: 32.5
+    },
+    {
+      logo: '/assets/logos/MySQL.webp',
+      title: 'MySQL',
+      level: 'Principiante',
+      count: 32.5
+    }
+  ]
   return (
-    <div>Skills</div>
+    <section id='skills' className='py-10 backdrop-filter backdrop-blur-lg bg-gray-300/40 relative'>
+      <div className='mt-2 text-black text-center'>
+        <h3 className='text-4xl font-[Tussila]'>
+          Habilidades
+        </h3>
+        <div className='flex items-center justify-center mt-12 gap-10 flex-wrap'>
+          {
+           skills?.map((skill, i) => (
+             <div key={i} className='border-2 group border-gray-200/50 relative min-w-[10rem] max-w-[16rem] backdrop-filter backdrop-blur-lg bg-gray-400/30 p-10 rounded-xl'>
+               <div style={{ background: `conic-gradient(rgba(62,143,239, 0.6) ${skill.count}%,#ddd ${skill.count}%)` }} className='w-32 h-32 flex items-center justify-center rounded-full'>
+                 <div className='text-6xl w-28 h-28 bg-gray-300 rounded-full flex items-center justify-center group-hover:text-yellow-500'>
+                   <img src={skill.logo} alt={skill.title} title={skill.title} className='select-none' />
+                 </div>
+               </div>
+               <p className='font-[Sequel] text-sm mt-3'>{skill.level}</p>
+             </div>
+           ))
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
