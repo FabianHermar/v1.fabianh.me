a4319eaf4c3c210bc9154e6c16dea1e05875f482
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-02-03 14:43:08 -0600

    🆕 projects component added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,76 @@
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className='py-10'>
+      <h2 className='text-4xl font-[Tussila] drop-shadow-lg text-center pb-4'>Proyectos</h2>
+      <div className='flex flex-col lg:flex-row justify-center items-center lg:justify-around'>
+        <div className='mt-2'>
+          <img alt='Home' src='https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' className='h-56 w-auto rounded-md object-cover' />
+          <div>
+            <div className='flex gap-2 pt-2'>
+              <span className='inline-block bg-blue-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] text-blue-900 hover:shadow-sm duration-150 hover:scale-105'>ReactJS</span>
+              <span className='inline-block bg-pink-400 bg-opacity-100 rounded px-1 py-1 text-sm font-[FBB] text-pink-900 hover:shadow-sm duration-150 hover:scale-105'>TailwindCSS</span>
+              <span className='inline-block bg-yellow-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] text-yellow-900 hover:shadow-sm duration-150 hover:scale-105'>JavaScript</span>
+            </div>
+          </div>
+          <div>
+            <h2 className='text-2xl pt-3 font-[Tussila] font-extrabold'>Cinnamon Overoll</h2>
+            <p className='font-[FBB]'>Sitio web de venta de rollos de canela, <br /> snacks y bebidas</p>
+            <div className='flex gap-2 pt-5'>
+              <a href='http://visual-tradex.com' target='_blank' rel='noopener noreferrer'>
+                <img src='https://img.shields.io/badge/Repo-100000?style=for-the-badge&logo=github&logoColor=white' alt='Repo' title='Repository' />
+              </a>
+              <a href='http://github.com' target='_blank' rel='noopener noreferrer'>
+                <img src='https://img.shields.io/badge/Deploy-000000?style=for-the-badge&logo=vercel&logoColor=white' alt='Live Demo' title='Live Demostration' />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='mt-2'>
+          <img alt='Home' src='https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' className='h-56 w-auto rounded-md object-cover' />
+          <div>
+            <div className='flex gap-2 pt-2'>
+              <span className='inline-block bg-blue-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] text-blue-900 hover:shadow-sm duration-150 hover:scale-105'>ReactJS</span>
+              <span className='inline-block bg-pink-400 bg-opacity-100 rounded px-1 py-1 text-sm font-[FBB] text-pink-900 hover:shadow-sm duration-150 hover:scale-105'>TailwindCSS</span>
+              <span className='inline-block bg-yellow-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] text-yellow-900 hover:shadow-sm duration-150 hover:scale-105'>JavaScript</span>
+            </div>
+          </div>
+          <div>
+            <h2 className='text-2xl pt-3 font-[Tussila] font-extrabold'>Cinnamon Overoll</h2>
+            <p className='font-[FBB]'>Sitio web de venta de rollos de canela, <br /> snacks y bebidas</p>
+            <div className='flex gap-2 pt-5'>
+              <a href='http://visual-tradex.com' target='_blank' rel='noopener noreferrer'>
+                <img src='https://img.shields.io/badge/Repo-100000?style=for-the-badge&logo=github&logoColor=white' alt='Repo' title='Repository' />
+              </a>
+              <a href='http://github.com' target='_blank' rel='noopener noreferrer'>
+                <img src='https://img.shields.io/badge/Deploy-000000?style=for-the-badge&logo=vercel&logoColor=white' alt='Live Demo' title='Live Demostration' />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='mt-2'>
+          <img alt='Home' src='https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' className='h-56 w-auto rounded-md object-cover' />
+          <div>
+            <div className='flex gap-2 pt-2'>
+              <span className='inline-block bg-blue-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] text-blue-900 hover:shadow-sm duration-150 hover:scale-105'>ReactJS</span>
+              <span className='inline-block bg-pink-400 bg-opacity-100 rounded px-1 py-1 text-sm font-[FBB] text-pink-900 hover:shadow-sm duration-150 hover:scale-105'>TailwindCSS</span>
+              <span className='inline-block bg-yellow-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] text-yellow-900 hover:shadow-sm duration-150 hover:scale-105'>JavaScript</span>
+            </div>
+          </div>
+          <div>
+            <h2 className='text-2xl pt-3 font-[Tussila] font-extrabold'>Cinnamon Overoll</h2>
+            <p className='font-[FBB]'>Sitio web de venta de rollos de canela, <br /> snacks y bebidas</p>
+            <div className='flex gap-2 pt-5'>
+              <a href='http://visual-tradex.com' target='_blank' rel='noopener noreferrer'>
+                <img src='https://img.shields.io/badge/Repo-100000?style=for-the-badge&logo=github&logoColor=white' alt='Repo' title='Repository' />
+              </a>
+              <a href='http://github.com' target='_blank' rel='noopener noreferrer'>
+                <img src='https://img.shields.io/badge/Deploy-000000?style=for-the-badge&logo=vercel&logoColor=white' alt='Live Demo' title='Live Demostration' />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
