03efca3f8cb8931f6a32c6272481b65096cb27c3
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-02-03 18:12:58 -0600

    🔁 images resized inmd screen

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
       <h2 className='text-4xl font-[Tussila] drop-shadow-lg text-center pb-4'>Proyectos</h2>
       <div className='flex flex-col lg:flex-row justify-center items-center lg:justify-around gap-6'>
         <div className='mt-2'>
-          <img alt='Cinnamon Overoll' src={CO} className='h-56 w-auto rounded-md object-cover' />
+          <img alt='Cinnamon Overoll' src={CO} className='h-52 md:h-56 md:w-auto rounded-md object-cover' />
           <div>
             <div className='flex gap-2 pt-2'>
               <span className='inline-block bg-orange-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] font-bold text-orange-900 hover:shadow-sm duration-150 hover:scale-105'>Astro</span>
@@ -18,7 +18,7 @@ const Projects = () => {
           </div>
           <div>
             <h2 className='text-2xl pt-3 font-[Tussila] font-extrabold'>Cinnamon Overoll</h2>
-            <p className='font-[FBB]'>Sitio web de venta de rollos de canela, <br /> snacks y bebidas</p>
+            <p className='font-[FBB] pl-[2px] md:pl-0'>Sitio web de venta de rollos de canela, <br /> snacks y bebidas</p>
             <div className='flex gap-2 pt-5'>
               <a href='https://github.com/FabianHMzz/Cinnamon-Overoll' target='_blank' rel='noopener noreferrer' className='duration-150 hover:scale-105 hover:shadow-sm'>
                 <img src='https://img.shields.io/badge/Repo-100000?style=for-the-badge&logo=github&logoColor=white' alt='Repo' title='Repository' />
@@ -31,7 +31,7 @@ const Projects = () => {
         </div>
 
         <div className='mt-2'>
-          <img alt='School System Design' src={SSD} className='h-56 w-auto rounded-md object-cover' />
+          <img alt='School System Design' src={SSD} className='h-52 md:h-56 md:w-auto rounded-md object-cover' />
           <div>
             <div className='flex gap-2 pt-2'>
               <span className='inline-block bg-orange-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] font-bold text-orange-900 hover:shadow-sm duration-150 hover:scale-105'>HTML5</span>
@@ -41,7 +41,7 @@ const Projects = () => {
           </div>
           <div>
             <h2 className='text-2xl pt-3 font-[Tussila] font-extrabold'>Control Escolar</h2>
-            <p className='font-[FBB]'>Diseño de sistema de <br /> control escolar</p>
+            <p className='font-[FBB] pl-[2px] md:pl-0'>Diseño de sistema de <br /> control escolar</p>
             <div className='flex gap-2 pt-5'>
               <a href='https://github.com/FabianHMzz/School-System-Design' target='_blank' rel='noopener noreferrer' className='duration-150 hover:scale-105 hover:shadow-sm'>
                 <img src='https://img.shields.io/badge/Repo-100000?style=for-the-badge&logo=github&logoColor=white' alt='Repo' title='Repository' />
@@ -54,7 +54,7 @@ const Projects = () => {
         </div>
 
         <div className='mt-2'>
-          <img alt='Home' src={DGT} className='h-56 w-auto rounded-md object-cover' />
+          <img alt='Home' src={DGT} className='h-52 md:h-56 md:w-auto rounded-md object-cover' />
           <div>
             <div className='flex gap-2 pt-2'>
               <span className='inline-block bg-orange-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] font-bold text-orange-900 hover:shadow-sm duration-150 hover:scale-105'>HTML5</span>
@@ -63,7 +63,7 @@ const Projects = () => {
           </div>
           <div>
             <h2 className='text-2xl pt-3 font-[Tussila] font-extrabold'>D'Gabbiani</h2>
-            <p className='font-[FBB]'>Sitio web de venta y renta de trajes <br /> y smokings</p>
+            <p className='font-[FBB] pl-[2px] md:pl-0'>Sitio web de venta y renta de trajes <br /> y smokings</p>
             <div className='flex gap-2 pt-5'>
               <a href='https://github.com/FabianHMzz/D-Gabbiani-Trajes' target='_blank' rel='noopener noreferrer' className='duration-150 hover:scale-105 hover:shadow-sm'>
                 <img src='https://img.shields.io/badge/Repo-100000?style=for-the-badge&logo=github&logoColor=white' alt='Repo' title='Repository' />
