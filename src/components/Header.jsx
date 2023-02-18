554641f57c56091c7f1e81b0e14b2b7cf764717e
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-02-17 20:41:55 -0600

    🔁 navbar logo replaced

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import FHLogo from '../../assets/FH.webp'
+import FHLogo from '../../assets/FH.png'
 
 const Header = () => {
   const [sticky, setSticky] = useState(false)
@@ -25,8 +25,9 @@ const Header = () => {
   return (
     <nav className={`fixed w-full left-0 top-0 z-[999] font-[Tussila] ${sticky ? 'bg-white/50 backdrop-filter backdrop-blur-md duration-[.1s] text-black' : 'text-white'}`}>
       <div className='flex items-center justify-between'>
-        <div className='mx-7 select-none'>
-          <img src={FHLogo} alt='logo' className='w-18 h-10' />
+        <div className='mx-7 select-none flex justify-center items-center gap-3'>
+          <img src={FHLogo} alt='logo' className='w-18 h-10 drop-shadow-md' />
+          <span className='text-xl text-black drop-shadow-sm'>FH</span>
         </div>
         <div className={`${sticky ? 'md:bg-white/0 bg-black' : 'bg-white/50 backdrop-filter backdrop-blur-md'} text-black md:block hidden px-7 py-2.5 font-medium rounded-bl-full`}>
           <ul className='flex items-center gap-1 py-2 text-lg'>
