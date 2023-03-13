325a8a62b76a423658c9987382a37e9102ae7afd
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-13 17:09:08 -0600

    🗑️ label unnecessary removed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -85,10 +85,9 @@ const Projects = () => {
           <img alt='README Components' src={RC} className='h-52 md:h-56 md:w-auto rounded-md object-cover' loading='lazy' />
           <div>
             <div className='flex gap-2 pt-2'>
-              <span className='inline-block bg-orange-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] font-bold text-orange-900 hover:shadow-sm duration-150 hover:scale-105'>Astro</span>
+              <span className='inline-block bg-blue-500 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] font-bold text-blue-900 hover:shadow-sm duration-150 hover:scale-105'>ReactJS</span>
               <span className='inline-block bg-sky-400 bg-opacity-100 rounded px-1 py-1 text-sm font-[FBB] font-bold text-sky-900 hover:shadow-sm duration-150 hover:scale-105'>TailwindCSS</span>
               <span className='inline-block bg-yellow-400 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] font-bold text-yellow-900 hover:shadow-sm duration-150 hover:scale-105'>JavaScript</span>
-              <span className='inline-block bg-blue-500 bg-opacity-75 rounded px-1 py-1 text-sm font-[FBB] font-bold text-blue-900 hover:shadow-sm duration-150 hover:scale-105'>ReactJS</span>
             </div>
           </div>
           <div>
