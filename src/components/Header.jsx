a9521b9a3cb4f5f17705f70b498ef596a2ef1097
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-05-06 13:13:12 -0600

    new version soon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,35 +23,63 @@ const Header = () => {
   }, [])
 
   return (
-    <nav className={`fixed w-full left-0 top-0 z-[999] font-[Tussila] ${sticky ? 'bg-white/50 backdrop-filter backdrop-blur-md duration-[.1s] text-black' : 'text-white'}`}>
+    <nav
+      className={`fixed w-full left-0 top-0 z-[999] font-[Tussila] ${
+        sticky
+          ? 'bg-white/50 backdrop-filter backdrop-blur-md duration-[.1s] text-black'
+          : 'text-white'
+      }`}
+    >
+      <div>
+        <p className='text-white bg-gray-500 h-8 flex items-center justify-center'>
+          New version of the portfolio soon.
+        </p>
+      </div>
       <div className='flex items-center justify-between'>
         <div className='mx-7 select-none flex justify-center items-center gap-3'>
           <img src={FHLogo} alt='logo' className='w-18 h-10 drop-shadow-md' />
           <span className='text-xl text-black drop-shadow-sm'>FH</span>
         </div>
-        <div className={`${sticky ? 'md:bg-white/0 bg-black' : 'bg-white/50 backdrop-filter backdrop-blur-md'} text-black md:block hidden px-7 py-2.5 font-medium rounded-bl-full`}>
+        <div
+          className={`${
+            sticky
+              ? 'md:bg-white/0 bg-black'
+              : 'bg-white/50 backdrop-filter backdrop-blur-md'
+          } text-black md:block hidden px-7 py-2.5 font-medium rounded-bl-full`}
+        >
           <ul className='flex items-center gap-1 py-2 text-lg'>
-            {
-              menuLinks?.map((menu, i) => (
-                <li key={i} className='px-4 hover:text-sky-500 ease-in duration-150'>
-                  <a href={menu?.link}>{menu?.name}</a>
-                </li>
-              ))
-            }
+            {menuLinks?.map((menu, i) => (
+              <li
+                key={i}
+                className='px-4 hover:text-sky-500 ease-in duration-150'
+              >
+                <a href={menu?.link}>{menu?.name}</a>
+              </li>
+            ))}
           </ul>
         </div>
-        <div onClick={() => setMenu(!menu)} className={`z-[999] text-3xl cursor-pointer ${menu ? 'text-black' : 'text-black '} md:hidden m-5`}>
+        <div
+          onClick={() => setMenu(!menu)}
+          className={`z-[999] text-3xl cursor-pointer ${
+            menu ? 'text-black' : 'text-black '
+          } md:hidden m-5`}
+        >
           <ion-icon name='menu-sharp'>...</ion-icon>
         </div>
-        <div className={`md:hidden text-black absolute w-2/3 h-screen px-7 py-2 font-medium bg-white top-0 duration-300 ${menu ? 'right-0' : 'right-[-100%]'}`}>
+        <div
+          className={`md:hidden text-black absolute w-2/3 h-screen px-7 py-2 font-medium bg-white top-0 duration-300 ${
+            menu ? 'right-0' : 'right-[-100%]'
+          }`}
+        >
           <ul className='flex flex-col justify-center h-full gap-10 py-2 text-lg'>
-            {
-              menuLinks?.map((menu, i) => (
-                <li key={i} className='px-6 hover:text-sky-500 ease-in duration-150'>
-                  <a href={menu?.link}>{menu?.name}</a>
-                </li>
-              ))
-            }
+            {menuLinks?.map((menu, i) => (
+              <li
+                key={i}
+                className='px-6 hover:text-sky-500 ease-in duration-150'
+              >
+                <a href={menu?.link}>{menu?.name}</a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
